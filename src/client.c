a05eae6e7e8b1f42142066a6d2922e38e64f017e
melisilva <melissa.silva2001.ms@gmail.com>
2021-04-09 18:40:48 +0100

    Did preparation of data for the rest of program + initial loop (I think)-->left some notes

--- src/client.c
+++ src/client.c
@@ -1,16 +1,56 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "./utils.h"
 
-#include "utils.h"
+#define FIFONAME_SIZE 200
+int i;
 
+void* send_request(void *arg){
+}
 
+int main(int argc, char * argv[]) {
+    srand(time(NULL));
 
-int main() {
+    if(argc!=3) //c <-t nsecs> fifoname
+    {
+        printf("Not enough arguments\n");
+        return 1;
+    }
+
+    int nsecs=atoi(argv[1]);
+    char *fifoname=(char *)malloc(FIFONAME_SIZE);
+    strcpy(fifoname,argv[2]);
+    pthread_t tid;
+
+    time_t t0=time(NULL);
     
+    while(time(NULL)<t0+nsecs)
+    {
+        //should we have mutex here?
+
+        //create thread 
+        if(pthread_create(&tid, NULL, send_request, (void*)fifoname)!=0)
+        {
+            return 1;
+        }
+        if(pthread_detach(tid)!=0)
+        {
+            return 1;
+        }
+        //wait x ms to send another request
+        if(usleep(( 10 + rand() % 5 )*1000)==-1) /*tried with rand()%10 +1 but the intervals where very lil
+                                                 for nsecs=2-->rand()%10 + 1 produced 169 requests 
+                                                 for nsecs=2-->10+rand()%5 produced 106 requests*/
+        {
+            return 1;
+        }
+    }
+
+
     //sample
     message_t* m_1 = (message_t*) malloc(sizeof(message_t));
     message_builder(m_1,27,2,-1);
     log_operation(m_1,FAILD);
 
+    free(m_1);
+    free(fifoname);
     return 0;
 }
--- src/utils.h
+++ src/utils.h
@@ -1,8 +1,12 @@
 #ifndef UTILS_H_
 #define UTILS_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <time.h>
 
 typedef enum oper {
     IWANT,
